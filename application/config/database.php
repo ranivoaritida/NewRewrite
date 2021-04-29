905758789129d0ad7ee884ea14e2d68f3ba617bb
RANIVOARITIDA Sandy <ranivoaritiadasandy@gmail.com>
2021-04-29 11:37:50 +0300

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => '',
 	'password' => '',
 	'database' => '',
-	'dbdriver' => '',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
